a08f994841dff4470d34bc99ff1498f0304722b5
Ibne Nahian <evilprince2009@gmail.com>
2021-09-25 11:12:01 +0600

    form properties

--- TinyPicker/ApplicationForm.Designer.cs
+++ TinyPicker/ApplicationForm.Designer.cs
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // ApplicationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(316, 500);
+            this.MaximizeBox = false;
+            this.Name = "ApplicationForm";
+            this.Resizable = false;
+            this.Style = MetroFramework.MetroColorStyle.Orange;
+            this.Text = "Tiny Picker";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
