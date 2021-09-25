6762073dfcf12d5f29c537bf6f6de58211e9657a
Ibne Nahian <evilprince2009@gmail.com>
2021-09-25 12:04:24 +0600

    Interface

--- TinyPicker/ApplicationForm.Designer.cs
+++ TinyPicker/ApplicationForm.Designer.cs
@@ -29,45 +29,128 @@
         private void InitializeComponent()
         {
             this.colorSpectrum = new System.Windows.Forms.PictureBox();
-            this.rgbText = new System.Windows.Forms.Label();
-            this.selectedColor = new System.Windows.Forms.Label();
+            this.SelectedColorPropGroup = new System.Windows.Forms.GroupBox();
+            this.selectedColorPreview = new System.Windows.Forms.Panel();
+            this.bluebox = new System.Windows.Forms.TextBox();
+            this.greenbox = new System.Windows.Forms.TextBox();
+            this.redbox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rgbValues = new System.Windows.Forms.TextBox();
+            this.colorPointer = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.colorSpectrum)).BeginInit();
+            this.SelectedColorPropGroup.SuspendLayout();
             this.SuspendLayout();
             // 
             // colorSpectrum
             // 
             this.colorSpectrum.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.colorSpectrum.Image = global::TinyPicker.Properties.Resources.Spectrum;
             this.colorSpectrum.Location = new System.Drawing.Point(23, 59);
             this.colorSpectrum.Name = "colorSpectrum";
-            this.colorSpectrum.Size = new System.Drawing.Size(270, 187);
+            this.colorSpectrum.Size = new System.Drawing.Size(228, 158);
+            this.colorSpectrum.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.colorSpectrum.TabIndex = 0;
             this.colorSpectrum.TabStop = false;
             // 
-            // rgbText
+            // SelectedColorPropGroup
             // 
-            this.rgbText.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.rgbText.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.rgbText.Location = new System.Drawing.Point(23, 249);
-            this.rgbText.Name = "rgbText";
-            this.rgbText.Size = new System.Drawing.Size(200, 24);
-            this.rgbText.TabIndex = 1;
+            this.SelectedColorPropGroup.Controls.Add(this.selectedColorPreview);
+            this.SelectedColorPropGroup.Controls.Add(this.bluebox);
+            this.SelectedColorPropGroup.Controls.Add(this.greenbox);
+            this.SelectedColorPropGroup.Controls.Add(this.redbox);
+            this.SelectedColorPropGroup.Controls.Add(this.label3);
+            this.SelectedColorPropGroup.Controls.Add(this.label2);
+            this.SelectedColorPropGroup.Controls.Add(this.label1);
+            this.SelectedColorPropGroup.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.SelectedColorPropGroup.Location = new System.Drawing.Point(23, 251);
+            this.SelectedColorPropGroup.Name = "SelectedColorPropGroup";
+            this.SelectedColorPropGroup.Size = new System.Drawing.Size(228, 100);
+            this.SelectedColorPropGroup.TabIndex = 3;
+            this.SelectedColorPropGroup.TabStop = false;
+            this.SelectedColorPropGroup.Text = "Selected Color";
             // 
-            // selectedColor
+            // selectedColorPreview
             // 
-            this.selectedColor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.selectedColor.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.selectedColor.Location = new System.Drawing.Point(229, 249);
-            this.selectedColor.Name = "selectedColor";
-            this.selectedColor.Size = new System.Drawing.Size(64, 24);
-            this.selectedColor.TabIndex = 2;
+            this.selectedColorPreview.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.selectedColorPreview.Location = new System.Drawing.Point(148, 18);
+            this.selectedColorPreview.Name = "selectedColorPreview";
+            this.selectedColorPreview.Size = new System.Drawing.Size(74, 75);
+            this.selectedColorPreview.TabIndex = 6;
+            // 
+            // bluebox
+            // 
+            this.bluebox.Location = new System.Drawing.Point(55, 70);
+            this.bluebox.Name = "bluebox";
+            this.bluebox.Size = new System.Drawing.Size(87, 23);
+            this.bluebox.TabIndex = 5;
+            // 
+            // greenbox
+            // 
+            this.greenbox.Location = new System.Drawing.Point(55, 44);
+            this.greenbox.Name = "greenbox";
+            this.greenbox.Size = new System.Drawing.Size(87, 23);
+            this.greenbox.TabIndex = 4;
+            // 
+            // redbox
+            // 
+            this.redbox.Location = new System.Drawing.Point(55, 18);
+            this.redbox.Name = "redbox";
+            this.redbox.Size = new System.Drawing.Size(87, 23);
+            this.redbox.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Blue :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Green :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Red :";
+            // 
+            // rgbValues
+            // 
+            this.rgbValues.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.rgbValues.Location = new System.Drawing.Point(23, 223);
+            this.rgbValues.Name = "rgbValues";
+            this.rgbValues.Size = new System.Drawing.Size(167, 23);
+            this.rgbValues.TabIndex = 4;
+            // 
+            // colorPointer
+            // 
+            this.colorPointer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.colorPointer.Location = new System.Drawing.Point(196, 223);
+            this.colorPointer.Name = "colorPointer";
+            this.colorPointer.Size = new System.Drawing.Size(55, 23);
+            this.colorPointer.TabIndex = 5;
             // 
             // ApplicationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(316, 500);
-            this.Controls.Add(this.selectedColor);
-            this.Controls.Add(this.rgbText);
+            this.ClientSize = new System.Drawing.Size(274, 370);
+            this.Controls.Add(this.colorPointer);
+            this.Controls.Add(this.rgbValues);
+            this.Controls.Add(this.SelectedColorPropGroup);
             this.Controls.Add(this.colorSpectrum);
             this.MaximizeBox = false;
             this.Name = "ApplicationForm";
@@ -75,15 +158,26 @@
             this.Style = MetroFramework.MetroColorStyle.Orange;
             this.Text = "Tiny Picker";
             ((System.ComponentModel.ISupportInitialize)(this.colorSpectrum)).EndInit();
+            this.SelectedColorPropGroup.ResumeLayout(false);
+            this.SelectedColorPropGroup.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox colorSpectrum;
-        private System.Windows.Forms.Label rgbText;
-        private System.Windows.Forms.Label selectedColor;
+        private System.Windows.Forms.GroupBox SelectedColorPropGroup;
+        private System.Windows.Forms.TextBox rgbValues;
+        private System.Windows.Forms.TextBox colorPointer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel selectedColorPreview;
+        private System.Windows.Forms.TextBox bluebox;
+        private System.Windows.Forms.TextBox greenbox;
+        private System.Windows.Forms.TextBox redbox;
     }
 }
 
--- TinyPicker/ApplicationForm.cs
+++ TinyPicker/ApplicationForm.cs
@@ -10,5 +10,6 @@ namespace TinyPicker
             InitializeComponent();
         }
 
+        
     }
 }
