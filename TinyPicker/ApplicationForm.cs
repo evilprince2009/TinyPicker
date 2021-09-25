d3a22bbf4c4876f081e704c632d55d2b80756847
Ibne Nahian <evilprince2009@gmail.com>
2021-09-25 11:07:11 +0600

    MetroForm

--- TinyPicker/ApplicationForm.cs
+++ TinyPicker/ApplicationForm.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MetroFramework.Forms;
 using System.Windows.Forms;
 
 namespace TinyPicker
 {
-    public partial class ApplicationForm : Form
+    public partial class ApplicationForm : MetroForm
     {
         public ApplicationForm()
         {
